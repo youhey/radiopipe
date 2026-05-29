6cee1e4a9fd3919c323fb7465275cbdcc4bc6d3f
IKEDA Youhei <youhey.ikeda@gmail.com>
2026-05-29 23:40:06 +0900

    fix: reorder admin navigation groups

--- src/app/Providers/Filament/AdminPanelProvider.php
+++ src/app/Providers/Filament/AdminPanelProvider.php
@@ -41,8 +41,8 @@ class AdminPanelProvider extends PanelProvider
             ->navigationGroups([
                 'Content',
                 'Configuration',
-                'Settings',
                 'Analysis',
+                'Settings',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
